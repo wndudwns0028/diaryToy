984c091bbc05e6e8c7f3f00cddc855fd1bb89e7f
KIM SEUNGSEOB <sub9707@naver.com>
2023-08-21 04:12:56 +0900

    공지 너비 조정

--- app/components/BootStrap/BoardTable.tsx
+++ app/components/BootStrap/BoardTable.tsx
@@ -8,7 +8,7 @@ export default function BoardTable({ boardList }: BoardTableProps) {
     <SSRProvider>
       <Table hover>
         <thead>
-          <tr>
+          <tr style={{ textAlign: "center" }}>
             <th style={{ width: "3%" }}>#</th>
             <th style={{ width: "65%" }}>제목</th>
             <th style={{ width: "10%" }}>작성일자</th>
@@ -18,10 +18,10 @@ export default function BoardTable({ boardList }: BoardTableProps) {
         <tbody>
           {boardList.map((board, i) => (
             <tr key={i}>
-              <td>{i + 1}</td>
+              <td style={{ textAlign: "center" }}>{i + 1}</td>
               <td>{board.title}</td>
-              <td>{board.date}</td>
-              <td>{board.views}</td>
+              <td style={{ textAlign: "center" }}>{board.date}</td>
+              <td style={{ textAlign: "center" }}>{board.views}</td>
             </tr>
           ))}
         </tbody>
