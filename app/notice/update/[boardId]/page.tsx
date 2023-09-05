d32479f126263350c6841522272c0fe49be25de7
KIM SEUNGSEOB <sub9707@naver.com>
2023-09-05 23:54:54 +0900

    fix Notice put Logic 생성

---
+++ app/notice/update/[boardId]/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+import SimpleModal from "@/app/components/Utils/SimpleModal";
+import styles from "../../write/page.module.scss";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { BoardType } from "@/types/boardTypes";
+import Link from "next/link";
+import { Button, Form } from "react-bootstrap";
+
+export default function Update({ params }: { params: { boardId: string } }) {
+  // state 변수 등록
+  const [boardData, setBoardData] = useState<BoardType>();
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const [titleText, setTitleText] = useState<string>(boardData?.title);
+  const [contentText, setContentText] = useState<string>(boardData?.content);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  // update 함수
+  function handleUpdate() {}
+
+  // fetch data
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      const res = await fetch(`/api/board/notice/${params.boardId}`);
+      const data = await res.json();
+      console.log("console" + data);
+      setBoardData(data);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.pageContainer}>
+      <SimpleModal
+        title="수정 성공"
+        message="게시글이 성공적으로 수정되었습니다."
+        show={isOpen}
+        onHide={() => {
+          setOpen(false);
+          router.push("/notice");
+        }}
+      />
+      <div className={styles.BannerWrapper}>
+        <h2 className={styles.BannerTitle}>공지사항 수정</h2>
+      </div>
+      <div className={styles.routeBox}>
+        <Link href={`/notice/${params.boardId}`}>뒤로가기</Link>
+      </div>
+      <div className={styles.writingBox}>
+        {isLoading ? (
+          <p>게시글 불러오는 중..</p>
+        ) : (
+          <>
+            <div className={styles.titleArea}>
+              <h2 className={styles.title}>글 제목</h2>
+              <Form.Control
+                size="lg"
+                type="text"
+                placeholder="공지 제목"
+                onChange={(e) => setTitleText(e.target.value)}
+                defaultValue={boardData?.title}
+              />
+            </div>
+            <div className={styles.writingArea}>
+              <h2 className={styles.title}>공지 내용</h2>
+              <div className={styles.editor}>
+                <Form.Control
+                  as="textarea"
+                  placeholder="글 내용 작성"
+                  style={{ height: "80vh" }}
+                  onChange={(e) => setContentText(e.target.value)}
+                  defaultValue={boardData?.content}
+                />
+              </div>
+              <div className={styles.fileAddArea}>첨부파일 : 준비중</div>
+            </div>
+          </>
+        )}
+        <div className={styles.btnGroup}>
+          <Button
+            variant="danger"
+            size="lg"
+            style={{ marginInline: "1%" }}
+            onClick={handleUpdate}
+          >
+            공지 수정
+          </Button>
+          <Button
+            variant="outline-danger"
+            size="lg"
+            style={{ marginInline: "1%" }}
+          >
+            취소하기
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
